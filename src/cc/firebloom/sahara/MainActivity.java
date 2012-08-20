c0c6881a197594419dd67633f63b8fea05842b41
Snow Hellsing <snow.hellsing@gmail.com>
2012-08-21 01:09:14 +0800

    NEW: settings UI

--- src/cc/firebloom/sahara/MainActivity.java
+++ src/cc/firebloom/sahara/MainActivity.java
@@ -1,24 +1,26 @@
 package cc.firebloom.sahara;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
-public class MainActivity extends Activity {
+public class MainActivity extends PreferenceActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        addPreferencesFromResource(R.xml.preferences);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.activity_main, menu);
+//        return true;
+//    }
 
     
 }
