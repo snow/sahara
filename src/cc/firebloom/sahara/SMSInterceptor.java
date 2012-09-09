7f30d0801cf098181e9a6f8464d61930eb5debfa
Snow Hellsing <snow.hellsing@gmail.com>
2012-09-09 17:00:38 +0800

    ING

--- src/cc/firebloom/sahara/SMSInterceptor.java
+++ src/cc/firebloom/sahara/SMSInterceptor.java
@@ -1,44 +1,74 @@
 package cc.firebloom.sahara;
 
-import cc.firebloom.sahara.filters.KeyworldFilter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
+import cc.firebloom.sahara.filters.SenderFilter;
 
 public class SMSInterceptor extends BroadcastReceiver {
-	private static final String TAG = "SMSInterceptor";
-	
-	@Override
-	public void onReceive(Context arg0, Intent arg1) {
-		Bundle inf = arg1.getExtras();
-	    
-	    if(null != inf){
-	      Object[] smsextras = (Object[]) inf.get( "pdus" );
+  private static final String TAG = "SMSInterceptor";
 
-	            for ( int i = 0; i < smsextras.length; i++ )
-	            {
-	                SmsMessage sms = SmsMessage.createFromPdu((byte[])smsextras[i]);
-	                
-	                // --- dev ---
-	                String strMsgBody = sms.getMessageBody().toString();
-	                String strMsgSrc = sms.getOriginatingAddress();
+  @Override
+  public void onReceive(Context ctx, Intent intend) {
+    Bundle inf = intend.getExtras();
 
-	                String strMessage = "SMS from " + strMsgSrc + " : " + strMsgBody;                    
+    if (null != inf) {
+      Object[] pdus = (Object[]) inf.get("pdus");
 
-	                Log.d(TAG, strMessage);
-	                // --- dev ---
-	                
-	                if(isSpam(sms)){
-	                	abortBroadcast();
-	                }
-	            }
-	    }
-	}
-	
-	public boolean isSpam(SmsMessage sms){
-		return KeyworldFilter.isSpam(sms);
-	}
+      for (int i = 0; i < pdus.length; i++) {
+        SmsMessage sms = SmsMessage.createFromPdu((byte[]) pdus[i]);
+
+        // --- dev ---
+        String strMsgBody = sms.getMessageBody().toString();
+        String strMsgSrc = sms.getOriginatingAddress();
+
+        String strMessage = "SMS from " + strMsgSrc + " : " + strMsgBody;
+
+        Log.d(TAG, "--------------------");
+        Log.d(TAG, strMessage);
+        // --- dev ---
+
+        if (isSpam(sms, ctx)) {
+          Log.d(TAG, "blocked!!");
+          abortBroadcast();
+          persistMessage(sms, ctx);
+        }
+      }
+    }
+  }
+
+  private boolean isSpam(SmsMessage sms, Context ctx) {
+    // return KeyworldFilter.isSpam(sms);
+    return SenderFilter.isSpam(sms, ctx);
+  }
+
+  private void persistMessage(SmsMessage sms, Context ctx) {
+    String yamlSkeleton = "---\n" + 
+                          "from: %s\n" + 
+                          "sent_at: %s\n" + 
+                          //"received_at: %s\n" + 
+                          "text: %s\n";
+    
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTimeInMillis(sms.getTimestampMillis());
+    DateFormat yamlFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS Z");
+    DateFormat filenameFormat = new SimpleDateFormat("yyyy-MM-dd_HHmm_ss_SSS");
+    
+    System.out.println(String.format(yamlSkeleton,
+                                     sms.getOriginatingAddress(), 
+                                     yamlFormat.format(calendar.getTime()),
+                                     sms.getDisplayMessageBody()));
+    
+    String filename = filenameFormat.format(calendar.getTime()) + "_" + 
+                        sms.getOriginatingAddress();
+    Log.d(TAG, filename);
+  }
 }
---
+++ src/cc/firebloom/sahara/filters/SenderFilter.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package cc.firebloom.sahara.filters;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.telephony.SmsMessage;
+import cc.firebloom.sahara.R;
+
+/**
+ * @author snow@firebloom.cc
+ *
+ */
+public class SenderFilter {
+  private static final String TAG = "SenderFilter";
+  
+  static public boolean isSpam(SmsMessage sms, Context ctx){   
+    String senderNumber = sms.getOriginatingAddress();
+    if(senderNumber.startsWith("+86")){
+      senderNumber = senderNumber.substring(3).trim();
+    }
+
+    Resources res = ctx.getResources();
+    String[] blocklist = res.getStringArray(R.array.init_blocklist);    
+    for(String blockNum:blocklist){
+      if(senderNumber.equalsIgnoreCase(blockNum)){
+        return true;
+      }
+    }
+    
+    return false;
+  }
+}
