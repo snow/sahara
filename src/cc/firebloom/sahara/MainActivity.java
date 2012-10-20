a33ff91c9f38d5d33696f9c514e48c58a216289a
Snow Hellsing <snow.hellsing@gmail.com>
2012-10-21 00:20:22 +0800

    CHG: use PreferenceFragment for API level since HONEYCOMB CHG: update introduction written in UI

--- src/cc/firebloom/sahara/MainActivity.java
+++ src/cc/firebloom/sahara/MainActivity.java
@@ -1,26 +1,31 @@
 package cc.firebloom.sahara;
 
+import android.annotation.TargetApi;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 public class MainActivity extends PreferenceActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        addPreferencesFromResource(R.xml.preferences);
-    }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.activity_main, menu);
-//        return true;
-//    }
-
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
     
+    if(VERSION_CODES.HONEYCOMB <= VERSION.SDK_INT) {
+      loadFragment();
+    } else {
+      addPreferencesFromResource(R.xml.preferences);
+    }
+  }
+  
+  // separate these codes into a method
+  // just for apply `@TargetApi(11)`
+  // which seens to can't be applied in onCreate()
+  @TargetApi(11)
+  protected void loadFragment() {
+    getFragmentManager().beginTransaction()
+      .replace(android.R.id.content, new SettingsFragment())
+      .commit();
+  }
 }
