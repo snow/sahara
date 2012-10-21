9f8d13bdf05b79721ac3c81d1eecbeb0cd05084e
Snow Hellsing <snow.hellsing@gmail.com>
2012-10-21 21:05:15 +0800

    NEW: spam list

---
+++ src/cc/firebloom/sahara/SpamArrayAdapter.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package cc.firebloom.sahara;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * @author snowhs
+ *
+ */
+public class SpamArrayAdapter extends ArrayAdapter<String> {
+  private final Context context;
+  private final List<String> filenames;
+  private final Map<String, File> name2file;
+  private final Yaml yaml;
+
+  public SpamArrayAdapter(Context context, List<String> filenames, Map<String, File> name2file) {
+    super(context, android.R.layout.two_line_list_item, filenames);
+    this.context = context;
+    this.filenames = filenames;
+    this.name2file = name2file;
+    this.yaml = new Yaml();
+  }
+  
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    LayoutInflater inflater = (LayoutInflater) context
+        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    View rowView = inflater.inflate(android.R.layout.two_line_list_item, parent, false);
+    
+    TextView titleV = (TextView)rowView.findViewById(android.R.id.text1);
+    TextView textV = (TextView)rowView.findViewById(android.R.id.text2);
+    
+    String filename = this.filenames.get(position);
+    File file = this.name2file.get(filename);
+    
+    InputStream is;
+    try {
+      is = new FileInputStream(file);
+      Map<String, Object> record = (Map<String, Object>) this.yaml.load(is);
+      
+      titleV.setText(record.get("from").toString());
+      textV.setText(record.get("text").toString());
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    
+    return rowView;
+  }
+}
---
+++ src/cc/firebloom/sahara/SpamListActivity.java
@@ -0,0 +1,66 @@
+package cc.firebloom.sahara;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.annotation.TargetApi;
+import android.app.ListActivity;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.os.Environment;
+
+public class SpamListActivity extends ListActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    //setContentView(R.layout.activity_spam_list);
+    if (VERSION_CODES.HONEYCOMB <= VERSION.SDK_INT){
+      configActionBar();
+    }
+
+    if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+      File extStorageDir = new File(Environment.getExternalStorageDirectory()
+          .getPath() + "/cc.firebloom.sahara/msg_bak");
+      if (extStorageDir.exists()) {
+        List<String> filenames = new ArrayList<String>();
+        Map<String, File> name2file = new HashMap<String, File>();
+        for(File file:extStorageDir.listFiles()){
+          String filename = file.getName();
+          filenames.add(filename);
+          name2file.put(filename, file);
+        }
+        Collections.sort(filenames, Collections.reverseOrder());
+        SpamArrayAdapter adapter = new SpamArrayAdapter(this, filenames, name2file);
+        setListAdapter(adapter);
+      }
+    }
+  }
+  
+  @TargetApi(11)
+  protected void configActionBar() {
+    getActionBar().setDisplayHomeAsUpEnabled(true);
+  }
+
+//  @Override
+//  public boolean onCreateOptionsMenu(Menu menu) {
+//    getMenuInflater().inflate(R.menu.activity_spam_list, menu);
+//    return true;
+//  }
+//
+//  @Override
+//  public boolean onOptionsItemSelected(MenuItem item) {
+//    switch (item.getItemId()) {
+//    case android.R.id.home:
+//      NavUtils.navigateUpFromSameTask(this);
+//      return true;
+//    }
+//    return super.onOptionsItemSelected(item);
+//  }
+
+}
